663923da693b0e9e289e5e9d6d6f81aeb82ff6b6
fosu562 <Franklino0808@gmail.com>
2022-11-27 11:22:49 +1300

    Editing resistor ratio calculator

--- pages/resistorRatios/resistorRatioCalc.js
+++ pages/resistorRatios/resistorRatioCalc.js
@@ -1,66 +1,12 @@
 // Define arrays for the different resistor ranges
 let personalResistorRange = new Array()
-let e12ResistorRange = new Array(
-  1.0,
-  1.2,
-  1.5,
-  1.8,
-  2.2,
-  2.7,
-  3.3,
-  3.9,
-  4.7,
-  5.6,
-  6.8,
-  8.2,
-  10,
-  12,
-  15,
-  18,
-  22,
-  27,
-  33,
-  39,
-  47,
-  56,
-  68,
-  82,
-  100,
-  120,
-  150,
-  180,
-  220,
-  270,
-  330,
-  390,
-  470,
-  560,
-  680,
-  820,
-  1000,
-  1200,
-  1500,
-  1800,
-  2200,
-  2700,
-  3300,
-  3900,
-  4700,
-  5600,
-  6800,
-  8200,
-  10000,
-  12000,
-  15000,
-  18000,
-  22000,
-  27000,
-  33000,
-  39000,
-  47000,
-  56000,
-  68000,
-  82000,
-)
+let e12ResistorRange = new Array(1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2)
 
-function myFunc() {}
+// Iterate for all of the multipliers
+for (let i = 0; i < 7; i++){
+  // Iterate through all of the values
+  for (let j = 0; j < 11; j++){
+    e12ResistorRange.push(e12ResistorRange[j] * (10 ^ i));
+  }
+}
+console.log(e12ResistorRange)
