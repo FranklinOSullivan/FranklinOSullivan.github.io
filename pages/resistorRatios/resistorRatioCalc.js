ce950b99431ca4d25dfc8fbe66d51249ceff66a1
fosu562 <Franklino0808@gmail.com>
2023-01-02 10:47:39 +1300

    Able to get and display the wanted value

--- pages/resistorRatios/resistorRatioCalc.js
+++ pages/resistorRatios/resistorRatioCalc.js
@@ -1,12 +1,10 @@
-// Define arrays for the different resistor ranges
-let personalResistorRange = new Array()
-let e12ResistorRange = new Array(1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2)
+const personalSeries = [0, 10, 22, 47, 100, 150, 200, 220, 270, 330, 470,
+  510, 680, 1000, 2000, 2200, 3300, 4700, 5100, 6800, 10000]
 
-// Iterate for all of the multipliers
-for (let i = 0; i < 7; i++){
-  // Iterate through all of the values
-  for (let j = 0; j < 11; j++){
-    e12ResistorRange.push(e12ResistorRange[j] * (10 ^ i));
-  }
+
+// Function to find the resistance values, called from button input
+function findResistanceValues() {
+  // Find the value wanted by the user
+  let wantedValue = Number(document.getElementById("wantedResistance").value);
+  alert(wantedValue);
 }
-console.log(e12ResistorRange)
